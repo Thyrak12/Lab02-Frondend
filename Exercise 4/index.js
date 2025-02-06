bd92c238845e19ea814b9962d0cdffa114fbda59
Thyrak12 <thyrak9167@gmail.com>
2025-02-06 22:14:53 +0700

    finish ex4

--- Exercise 4/index.js
+++ Exercise 4/index.js
@@ -1,13 +1,36 @@
+function tryAgain() {
+    document.getElementById('passwordView').style.display = 'block';
+    document.getElementById('lostView').style.display = 'none';
+    document.getElementById('wonView').style.display = 'none';
+    document.getElementById('passwordInput').value = '';
+}
+
+let count = 3;
+
 function comparePassword() {
     const x = document.getElementById('passwordInput').value;
     const password = '123';
     if (x == password) {
-        alert('Password is correct');
+        //show won view
+        document.getElementById('wonView').style.display = 'block';
+        document.getElementById('passwordView').style.display = 'none';
     }
     else {
-        alert('Password is incorrect');
+        document.getElementById('lostView').style.display = 'block';
+        document.getElementById('passwordView').style.display = 'none';
+        document.getElementById('tryAgainButton').addEventListener('click', tryAgain);
+        count--;
+        document.getElementById('instructionLabel').innerHTML = `Enter your code (You can try ${count} times only !)`;
+        if (count == 0) {
+            document.getElementById('tryAgainButton').remove();
+        }
+
     }
 };
 
+
+
 document.getElementById('checkButton').addEventListener('click', comparePassword);
+
+
 
